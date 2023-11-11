249c5019b63626b3e8678d705b1533ad2ad9c2e3
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-11 23:31:30 +0400

    Initial commit

---
+++ testing_program/testing_program.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    setlocale(0, "");
+
+    std::vector<float> collection;
+    int k = 0; 
+    float element;
+
+    while (std::cout << "\tВведите значение: ", std::cin >> element)
+    {
+        collection.push_back(element);
+        if (collection.back() == 0) 
+            break;  
+    }
+
+    std::cout << "\tКоллекция: ";
+    for (int i = 0; i < collection.size(); i++) 
+        std::cout << collection[i] << " | ";
+
+    std::cout << std::endl;
+    for (int i = 0; i < collection.size(); i++) 
+    {
+        if (collection[i] == 0)
+            break;
+        else if (collection[i] < collection[i + 1])
+            k++;
+    }
+
+    std::cout << "\tКолличесвто элементов, которые больше предыдущих: " << k;
+
+    return 0;
+}
